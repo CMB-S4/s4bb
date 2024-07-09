35e6a3de3e148f3ab3edf419bf86d5e9a08b9c74
Colin Bischoff <cbischoff@gmail.com>
2024-07-09 15:26:16 -0700

    Unit tests for bandpass.py

--- tests.py
+++ tests.py
@@ -6,8 +6,10 @@ Unit Tests
 """
 
 import unittest
+import numpy as np
 
 from spectra import MapDef, specind, mapind, specgen
+from bandpass import Bandpass
 
 class SpectraTest(unittest.TestCase):
     """
@@ -64,6 +66,31 @@ class BandpassTest(unittest.TestCase):
     Unit tests for bandpass.py
 
     """
+
+    def test_deltafn(self):
+        """Test delta-fn bandpass"""
+
+        nu0 = 100.0 # GHz
+        bp = Bandpass.deltafn(nu0)
+        self.assertEqual(1.0, bp.bandpass_integral(lambda x: 1.0))
+        self.assertEqual(nu0, bp.nu_eff())
+        # Check the CMB unit conversion
+        from bandpass import GHz, Tcmb, h, k, c
+        x = h * nu0 * GHz / (k * Tcmb)
+        conv = (2 * k**3 * Tcmb**2 / (h**2 * c**2) * x**4 * np.exp(x) /
+                (np.exp(x) - 1)**2)
+        self.assertEqual(conv, bp.cmb_unit_conversion())
+
+    def test_tophat(self):
+        """Test tophat bandpass"""
+
+        nu0 = 100.0 # GHz
+        nu1 = 120.0
+        bp = Bandpass.tophat(nu0, nu1, RJ=False)
+        self.assertEqual(1.0, bp.bandpass_integral(lambda x: 1.0))
+        self.assertEqual((nu0 + nu1) / 2, bp.nu_eff())
+        for nu in np.linspace(nu0, nu1, 10):
+            self.assertEqual(1.0 / (nu1 - nu0), bp.fn(nu))
 
 class BpwfTest(unittest.TestCase):
     """
