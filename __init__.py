62e851ef0eb93e6b28f7b613bf4ff67da339ea4e
Colin Bischoff <cbischoff@gmail.com>
2024-09-25 17:17:15 -0400

    Added __init__.py

---
+++ __init__.py
@@ -0,0 +1,15 @@
+"""
+====
+s4bb
+====
+
+Cross-spectrum analysis, likelihood, forecasting
+
+"""
+
+from util import mapind, specind, specgen, MapDef
+import bandpass
+import bpcov
+import bpwf
+import spectra
+
